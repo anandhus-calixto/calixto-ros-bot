7ca45f7ac591382c99a59939c82206fe98c69e4d
anandhus-calixto <anandhu.s@calixto.co.in>
2026-04-04 15:01:09 +0530

    still not stable

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -46,7 +46,7 @@ def generate_launch_description():
     spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-name', 'my_bot'
-                                   ,'-z', '0.1'],
+                                   ,'-z', '0.01'],
                         output='screen')
 
     # 2. Add the Bridge Node
