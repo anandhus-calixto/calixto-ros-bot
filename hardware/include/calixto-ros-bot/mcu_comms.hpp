1e54469d8d93e1d03bde32cf90176f62d5c408fc
anandhus-calixto <anandhu.s@calixto.co.in>
2026-04-21 12:46:26 +0530

    checkpoint

--- hardware/include/calixto-ros-bot/mcu_comms.hpp
+++ hardware/include/calixto-ros-bot/mcu_comms.hpp
@@ -53,14 +53,14 @@ public:
 
   std::string send_msg(const std::string &msg_to_send, bool print_output = false)
   {
-    //serial_conn_.FlushIOBuffers(); // Just in case
+    serial_conn_.FlushIOBuffers(); // Just in case
     serial_conn_.Write(msg_to_send);
 
     std::string response = "";
     try
     {
       // Responses end with \r\n so we will read up to (and including) the \n.
-      serial_conn_.ReadLine(response, '\r', timeout_ms_);
+      serial_conn_.ReadLine(response, '\n', timeout_ms_);
     }
     catch (const LibSerial::ReadTimeout&)
     {
